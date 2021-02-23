ae1b79fa2d98e3b75e70f86e0dfaa79723508859
Vlada-V29 <voroshilova.va@phystech.edu>
2021-02-23 14:38:38 +0300

    /

--- task-3/number-1.cpp
+++ task-3/number-1.cpp
@@ -13,7 +13,7 @@ void time_of_sort2(int);
 
 int main()
 {
-    const int N = 100; //~25
+    const int N = 100; //~25 -- changes
     time_of_sort1(N); 
     time_of_sort2(N);
 
@@ -22,29 +22,35 @@ int main()
 
 void time_of_sort1(const int n)
 {
-    std::vector<int> cont;
     auto t_start1 = std::chrono::system_clock::now();
+    std::vector<int> cont;
 
-    srand(5);//(static_cast<unsigned int>(t_start1));/////////////////////
+    auto rand_val = std::chrono::duration_cast<chrono::nanoseconds>(t_start1 - 
+        std::chrono::system_clock::now());
+    srand(rand_val.count());
+
     for(auto i = 1; i < n; ++i)
     {
         cont.push_back(rand());
     }
     sort(std::begin(cont), std::end(cont));
 
-    auto t_end1 = chrono::system_clock::now();
+    auto t_end1 = std::chrono::system_clock::now();
     auto result1 =
-    std::chrono::duration_cast<chrono::nanoseconds>(t_end1 - t_start1);
-    std::cout << "vector: " << result1.count() << " nc" << "\n";
+    std::chrono::duration_cast<chrono::microseconds>(t_end1 - t_start1);
+    std::cout << "vector: " << result1.count() << " mc" << "\n";
 
 }
 
 void time_of_sort2(const int n)
 {
-    std::set<int> cont;
     auto t_start2 = chrono::system_clock::now();
+    std::set<int> cont;
 
-    srand(5);
+    auto rand_val = std::chrono::duration_cast<chrono::microseconds>(t_start2 - 
+        std::chrono::system_clock::now());
+    srand(rand_val.count());
+
     for(auto i = 1; i < n; ++i)
     {
         cont.insert(rand());
@@ -52,7 +58,7 @@ void time_of_sort2(const int n)
 
     auto t_end2 = chrono::system_clock::now();
     auto result2 =
-    std::chrono::duration_cast<chrono::nanoseconds>(t_end2 - t_start2);
-    std::cout << "set: " << result2.count() << " nc" << "\n";
+    std::chrono::duration_cast<chrono::microseconds>(t_end2 - t_start2);
+    std::cout << "set: " << result2.count() << " mc" << "\n";
 
 }
