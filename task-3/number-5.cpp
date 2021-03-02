92f0c221e71bf2c4ed0aa8c9ffc573713ed97668
Vlada-V29 <voroshilova.va@phystech.edu>
2021-03-02 19:37:47 +0300

    .

--- task-3/number-5.cpp
+++ task-3/number-5.cpp
@@ -7,6 +7,8 @@
 #include <boost/multi_index/ordered_index.hpp>
 #include <boost/multi_index/random_access_index.hpp>
 
+#include <iomanip>
+
 using namespace std;
 using namespace boost::multi_index;
 
@@ -23,6 +25,9 @@ typedef boost::multi_index_container<Rec,
 			tag<Rec::ByName>, member<Rec, std::string, &Rec::name>
 		>, 
 		hashed_non_unique<
+			tag<Rec::ByPhone>, member<Rec, std::string, &Rec::phone>
+		>,
+		hashed_non_unique<
 			tag<Rec::ByAddr>, member<Rec, std::string, &Rec::addr>
 		>,
         random_access<>
@@ -34,16 +39,37 @@ std::size_t hash_value(std::string x) { return hash<string>{}(x); }
 int main()
 {
     Store store;
-	Rec r1 = { "Basilio Pupkinio", "022", "Neron st" };
+	Rec r1 = { "Basilio Pupkinio", "024", "Neron st" };
     Rec r2 = { "Vasya Pupkin",     "022", "Around st" };
     Rec r3  = { "Vasilisa Pupkina", "022", "Around st" };
+	Rec r4  = { "David Tennant", "010", "Abby Road" };
+	Rec r5  = { "Michael Sheen", "399", "Carnaby st" };
 	store.insert(r1);
 	store.insert(r2);
     store.insert(r3);
+	store.insert(r4);
+	store.insert(r5);
 
-	//???
-    
-    
+
+	for (auto it:store)	
+	{
+		std::cout << it.name << ", " << (it).phone << ", " 
+			<< (it).addr << "; " << std::endl;
+	}
+	std::cout << "\n" << std::endl;
+
+
+	std::string find_id = "David Tennant"; 
+	typedef Store::index<Rec::ByName>::type List;
+	const List& ls1 = store.get<Rec::ByName>();
+	List::const_iterator it1 = ls1.find(find_id);
+	if (it1 != ls1.end())
+	{
+		std::cout << "David Tennant\t" << it1->phone << "\t" 
+			<< it1->addr << std::endl;
+	}
+	std::cout << "\n" << std::endl;
+	 
     
     return 0;
 }
