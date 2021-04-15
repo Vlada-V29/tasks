af2e0e868dd938dedd9d8084afbeb95c649af88f
Vlada-V29 <voroshilova.va@phystech.edu>
2021-04-15 11:48:04 +0300

    уже лучше

--- task-9/number-1.cpp
+++ task-9/number-1.cpp
@@ -1,7 +1,7 @@
 //#define BOOST_DATE_TIME_NO_LIB
 
 #include <iostream>
-#include <string>//std::pair<int, std::string>
+#include <string>
 #include <utility>
 #include <chrono>
 #include<thread>
@@ -46,7 +46,7 @@ void print_mes(MyMap *mymap, int* flag)
 }
 
 
-void enter_mes(MyMap *mymap, const std::chrono::system_clock::time_point *tp, bool* flag)
+void enter_mes(MyMap *mymap, const std::chrono::system_clock::time_point *tp, int* flag)
 {
 	while (true)
 	{
@@ -76,34 +76,15 @@ void enter_mes(MyMap *mymap, const std::chrono::system_clock::time_point *tp, bo
 
 int main(int argc, char ** argv)
 {
-	//using allocator = boost::interprocess::allocator < std::pair<int, std::string>,
-	//	boost::interprocess::managed_shared_memory::segment_manager > ;
-
-	//using cont = boost::interprocess::basic_string < , 
-	//	std::char_traits < char >, allocator> ;
-
-	//using cont = map<int, std::string>;
-
+	
 	const std::string shared_memory_name = "vk";
-	const std::string flag_name = "flag";
-
+	
 	boost::interprocess::shared_memory_object::remove(shared_memory_name.c_str());//?
-	boost::interprocess::shared_memory_object::remove(flag_name.c_str());
-
+	
 	managed_shared_memory segment(
 		open_or_create, shared_memory_name.c_str(), 1024);
-	//managed_shared_memory seg_flag(
-	//	open_or_create, flag_name.c_str(), 10);
-
-	//typedef int KeyType;
-/*	typedef int KeyType;
-	typedef std::string MappedType;
-	typedef std::pair<const int, std::string> ValueType;
-
-	typedef allocator<ValueType, managed_shared_memory::segment_manager>
-		ShmemAllocator;
-	typedef map<KeyType, MappedType, std::less<KeyType>, ShmemAllocator> MyMap;
-*/
+	
+	
 	ShmemAllocator alloc_inst (segment.get_segment_manager());
 
 	MyMap *mymap =
@@ -111,64 +92,31 @@ int main(int argc, char ** argv)
 			(std::less<int>() //first ctor parameter
 			,alloc_inst); //second ctor parameter
 
-	//print_map("Updated map: ", m);
+	// print_map("Updated map: ", mymap);
 	
 	int *flag = segment.find_or_construct<int>("Integer")(0);
+	//int *number_of_users = segment.find_or_construct<int>("Integer")(1);
 	const std::chrono::system_clock::time_point *tp = segment.find_or_construct
 		<std::chrono::system_clock::time_point>("clock")(std::chrono::system_clock::now());
-	// auto tp = std::chrono::system_clock::now();
+	
+	auto iterator =  mymap->begin();
+	for(auto i = 0; i < *flag; ++i)
+	{
+		std::cout << (++iterator)->first << ":\t" <<
+				(++iterator)->second << std::endl;
+	}
+	
 
-
-	// auto s = segment.find_or_construct < cont > ("lala")
-	// 	//(std::pair<int, std::string>(1, "kvbkb"),
-	// 	pair<int, std::string>(1, "kvbkb"),
+	
 	// 	shared_memory.get_segment_manager());
 
-	//s->push_back(cont(2, "jhg"));
-
-	//std::cout << 45 << std::endl;
-
-	//Insert data in the map
-	/*
-	for(int i = 0; i < 10; ++i)
-	{
-		mymap->insert(std::pair<KeyType, MappedType>(i, "bfbkj"));
-	}
-*/
-	
-
-	// using std::chrono::operator""s;
-	// std::this_thread::sleep_until(std::chrono::system_clock::now() + 1s);
 
 
+	std::thread th_enter;
+	std::thread th_print;
 
-
-//something is as wrong as possible, but I don't understand what to do
-/*	while(1)
-	{
-		if(*flag)
-		{
-			std::cout << (mymap->end())->first << ":\t" <<
-				(mymap->end())->second << std::endl;
-		}
-		MappedType str;
-		std::cin >> str;
-		*flag = 1;
-		// std::getline(std::in, str);
-		auto tp2 = std::chrono::duration_cast<std::chrono::seconds>
-			(std::chrono::system_clock::now() - tp);
-		mymap->insert(std::pair<KeyType, MappedType>(tp2.count(), str));
-		//mymap->
-		std::cout << (mymap->end())->first << ":\t" <<
-			(mymap->end())->second << std::endl;
-		*flag = 0;
-	}
-	
-*/
-
-
-
-
+	th_enter = std::thread(enter_mes, mymap, tp, flag); 
+	th_print = std::thread(print_mes, mymap, flag); 
 
 
 
